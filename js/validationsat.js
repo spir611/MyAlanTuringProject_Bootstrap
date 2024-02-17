ccfd85598e3c5da3619570a31b1670eef3d68750
Sandra Pirpamer <spir611@g.educaand.es>
2024-02-17 03:05:36 +0100

    Add changes to largescreen config

---
+++ js/validationsat.js
@@ -0,0 +1,54 @@
+/******************************************************************************/
+/*                                                                            */
+/*          Javascript for validations Alan Turing Project with Bootstrap     */
+/*          Author: Sandra Barbara Pirpamer                                   */
+/*          Version: 1.0                                                      */
+/*          Date: FEBRERO 2024                                                */
+/*          Comments: Lenguaje de Marcas Project Bootstrap 2Trimestre         */
+/*                                                                            */   
+/******************************************************************************/
+
+function validacion(){
+    let correcto=true;
+    let nombre=document.getElementById('nombre');
+    let apellidos=document.getElementById('apellidos');
+    let email=document.getElementById('email');
+    let conditions=document.getElementById('conditions');
+    
+    if (nombre.value.length==0 || /^\s+$/.test(nombre.value)){
+        correcto=false;
+        nombre.style.borderColor="red";
+        document.getElementById('nombreHelp').style.visibility="visible";
+    }
+
+      /* console.log(apellidos.value+ /^\s+$/.test(apellidos.value)); */
+    if (apellidos.value.length==0 || /^\s+$/.test(apellidos.value)){
+        correcto=false;
+        apellidos.style.borderColor="red";
+        document.getElementById('apellidosHelp').style.visibility="visible";
+    }
+
+    if (!(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.value))){
+        correcto=false;
+        email.style.borderColor="red";
+        document.getElementById('emailHelp').style.visibility="visible";
+    }
+
+    if (conditions.value==0){
+        correcto=false;
+        conditions.style.borderColor="red";
+        document.getElementById('conditionsHelp').style.visibility="visible";
+    }
+
+    
+    return correcto;
+
+
+}
+
+function resetear(id)
+    {   document.getElementById(id+'Help').style.visibility="hidden";
+        document.getElementById(id).style.borderColor="lightgray";
+    }
+
+    
